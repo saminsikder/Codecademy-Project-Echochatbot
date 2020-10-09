b50fa6d7bca9b067f80e6f6e860aa23b6b6777b0
Samin <saminsikder42@gmail.com>
2020-10-09 12:12:47 +0600

    Echo Chatbot 1 Done

--- echochatbot.py
+++ echochatbot.py
@@ -1,2 +1,7 @@
-greeting = input("Hello baby Samin")
+print("Welcome to Samin's first chatbot")
 
+print("I will repeat after you.")
+
+stuff_to_echo = input("Enter something for me to repeat: " " ")
+
+print("You said: " + stuff_to_echo)
